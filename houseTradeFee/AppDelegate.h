419ece904ec9413c6c43c53e959b63b0ce315682
Songsong Huang <soso_huang@kingdee.com>
2012-05-24 20:44:53 +0800

    Initial Commit

---
+++ houseTradeFee/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  houseTradeFee
+//
+//  Created by  on 12-5-24.
+//  Copyright (c) 2012年 iSoso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ houseTradeFee/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  houseTradeFee
+//
+//  Created by  on 12-5-24.
+//  Copyright (c) 2012年 iSoso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
